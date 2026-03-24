84d6826c6b61625e5e95dbe57df60962106827de
Bharaniharan Ashokkumar <bharaniharana@infinite.com>
2026-03-24 09:19:51 +0530

    first commit

---
+++ CSharp/FirstConsole/FirstConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FirstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            test t2= new test();
+            
+
+        }
+    }
+    class test
+    {
+        public static void main()
+        {
+            Console.WriteLine("Hiiiii");
+        }
+    }
+}
---
+++ CSharp/FirstConsole/FirstConsole/TypeConvertion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    internal class TypeConvertion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The min value is : {int.MinValue}");
+        }
+    }
+}
