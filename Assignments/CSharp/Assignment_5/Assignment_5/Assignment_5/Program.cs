b3a4935e9eb99296c1bc65018de99424bf818820
Bharaniharan Ashokkumar <bharaniharana@infinite.com>
2026-04-05 17:58:07 +0530

    Assignment 5

---
+++ Assignments/CSharp/Assignment_5/Assignment_5/Assignment_5/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Assignment_5
+{
+    public class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException(string message) : base(message)
+        {
+        }
+    }
+    class BankingSystem
+    {
+        private double balance=0;
+        
+        public void DepositMoney(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount}");
+            }
+            else
+            {
+                throw new ArgumentException("Amount must be Greater than zero..");
+            }
+        }
+
+        public void WithdrawMoney(double amount)
+        {
+            if (amount > 0)
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    Console.WriteLine($"Withdrawn: {amount}");
+                }
+                else
+                {
+                    throw new InsufficientFundsException("Insufficient funds.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Amount must be Greater than zero..");
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Current Balance: {balance}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankingSystem bank = new BankingSystem();
+            
+            try
+            {
+                int n = 0;
+                do
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Banking Services");
+                    Console.WriteLine("1. Deposit Money");
+                    Console.WriteLine("2. Withdraw Money");
+                    Console.WriteLine("3. Check Balance");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("Enter the service: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    double Amount;
+                    switch (n)
+                    {
+                        case 1:
+                            Console.Write("Enter the amount: " );
+                            Amount = Convert.ToDouble(Console.ReadLine());
+                            bank.DepositMoney(Amount);
+
+                            break;
+                        case 2:
+                            Console.Write("Enter the amount: ");
+                            Amount = Convert.ToDouble(Console.ReadLine());
+                            bank.WithdrawMoney(Amount);
+                            break;
+                        case 3:
+                            bank.CheckBalance();
+                            break;
+                        case 4:
+                            Console.WriteLine("Exited....");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input...");
+                            break;
+                    }
+                } while (n != 4);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ Assignments/CSharp/Assignment_5/Assignment_5/Assignment_5/ScolarshipClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    class UserException : Exception
+    {
+        public UserException(string message) : base(message) { }
+    }
+    class Scolarship
+    {
+        int mark;
+        double fees;
+        public Scolarship(int mark, double fees)
+        {
+            this.mark = mark;
+            this.fees = fees;
+        }
+        public void Merit()
+        {
+            if (this.mark > 70 && this.mark <= 80)
+                Console.WriteLine("Scolarship amount: " + this.fees * 0.2);
+            else if (this.mark > 80 && this.mark <= 90)
+                Console.WriteLine("Scolarship amount: " + this.fees * 0.3);
+            else if (this.mark > 90 && this.mark <= 100)
+                Console.WriteLine("Scolarship amount: " + this.fees * 0.5);
+            else if (this.mark > 100 || this.mark < 0)
+                throw new UserException("Invalid mark....");
+            else
+                throw new UserException("Your not eligile for Scolarship....");
+
+        }
+    }
+    internal class ScolarshipClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Scolarship serivces");
+            Console.Write("Enter your mark (out of 100): ");
+            int mark= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your fees : ");
+            int fees = Convert.ToInt32(Console.ReadLine());
+            Scolarship s = new Scolarship(mark,fees);
+            try
+            {
+                s.Merit();
+            }
+            catch (UserException ue)
+            {
+                Console.WriteLine(ue.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+        }
+    }
+}
